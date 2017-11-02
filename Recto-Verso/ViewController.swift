5ce460173207ea81350868656dc1198ee8ec8f71
eleves <eleves@marcospaulo.local>
2017-11-02 09:05:21 -0400

    Initial Commit

---
+++ Recto-Verso/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recto-Verso
+//
+//  Created by eleves on 17-11-02.
+//  Copyright © 2017 Reverso-Softissimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
